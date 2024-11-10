aa060bfeeaf7e861956266ec0bf9826f9b821e19
DanielReker <sasha29072004@gmail.com>
2024-11-11 01:34:03 +0400

    Add simple app description to "About"

--- src/components/widgets/AboutWidget.jsx
+++ src/components/widgets/AboutWidget.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Box, Link, List, ListItem, Typography} from "@mui/material";
 
+// TODO: Rewrite about or move to some .md file
 const AboutWidget = () => {
     return (
         <Box>
-            Work in progress...
+            <Typography variant='h6' gutterBottom>What's this application for?</Typography>
+            <Typography gutterBottom>
+                This is Unturned Helper — an application used to explore Unturned and its maps,
+                both official and curated. That means, you can easily find and view assets data (different params of
+                Unturned items and other stuff), see exact airdrops locations on your favorite map, explore objects on
+                map and much more.
+            </Typography>
+
+            <Typography variant='h6' gutterBottom>Open source</Typography>
+            <Typography gutterBottom>
+                Unturned Helper is open source — that means you can always find its source code and serialized
+                Unturned data in following GitHub repositories:<br/>
+                <Link href='https://github.com/DanielReker/unturned-helper' target='_blank'>
+                    https://github.com/DanielReker/unturned-helper
+                </Link><br/>
+                <Link href='https://github.com/DanielReker/unturned-data-serializer' target='_blank'>
+                    https://github.com/DanielReker/unturned-data-serializer
+                </Link><br/>
+                <Link href='https://github.com/DanielReker/unturned-serialized-data' target='_blank'>
+                    https://github.com/DanielReker/unturned-serialized-data
+                </Link><br/>
+            </Typography>
+
+            <Typography variant='h6' gutterBottom>Work in progress...</Typography>
         </Box>
     );
 };
